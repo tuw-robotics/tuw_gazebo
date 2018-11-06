bfb0fc2293f9f49564de737a07eae2ce7f62c052
Markus Bader <markus.bader@tuwien.ac.at>
2018-11-06 11:08:27 +0100

    legacy mode removed

--- tuw_gazebo_plugins/include/tuw_gazebo_plugins/gazebo_ros_diff_drive.h
+++ tuw_gazebo_plugins/include/tuw_gazebo_plugins/gazebo_ros_diff_drive.h
@@ -127,7 +127,6 @@ namespace gazebo {
       std::string odometry_frame_;
       std::string robot_base_frame_;
       bool publish_tf_;
-      bool legacy_mode_;
       // Custom Callback Queue
       ros::CallbackQueue queue_;
       boost::thread callback_queue_thread_;
