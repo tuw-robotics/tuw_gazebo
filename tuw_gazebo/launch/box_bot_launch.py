51d4c3b01790dc3f384f40bd386e768cf886ed45
Markus Bader <markus.bader@mx-robotics.com>
2022-06-30 15:23:58 +0200

    first commit

---
+++ tuw_gazebo/launch/box_bot_launch.py
@@ -0,0 +1,30 @@
+f#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_box_car_gazebo = get_package_share_directory('box_car_gazebo')
+    pkg_box_car_description = get_package_share_directory('box_car_description')
+
+    # Sart World
+    start_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_box_car_gazebo, 'launch', 'start_world_launch.py'),
+        )
+    )  
+
+    return LaunchDescription([
+        start_world,
+        spawn_robot_world
+    ])
