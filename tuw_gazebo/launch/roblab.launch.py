4ce025a0f96680d7aa842ea4c53636d25063db1a
Markus Bader <markus.bader@mx-robotics.com>
2022-07-21 15:15:26 +0200

    screenshot added

--- tuw_gazebo/launch/roblab.launch.py
+++ tuw_gazebo/launch/roblab.launch.py
@@ -6,18 +6,22 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.substitutions import TextSubstitution
+from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_tuw_gazebo = get_package_share_directory('tuw_gazebo')
+    pkg_tuw_gazebo_models = get_package_share_directory('tuw_gazebo_models')
+    pkg_tuw_gazebo_plugins = get_package_share_directory('tuw_gazebo_plugins')
 
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        model_path =  os.environ['GAZEBO_MODEL_PATH'] + ':' + pkg_tuw_gazebo_models + '/models'
+    else:
+        model_path =  pkg_tuw_gazebo_models + '/models'
 
     # Gazebo launch
     gazebo = IncludeLaunchDescription(
@@ -28,9 +32,14 @@ def generate_launch_description():
 
 
     return LaunchDescription([
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
         DeclareLaunchArgument(
           'world',
           default_value=[os.path.join(pkg_tuw_gazebo, 'worlds', 'roblab.world'), ''],
+          description='SDF world file'),
+        DeclareLaunchArgument(
+          'verbose',
+          default_value='true',
           description='SDF world file'),
         gazebo
     ])
