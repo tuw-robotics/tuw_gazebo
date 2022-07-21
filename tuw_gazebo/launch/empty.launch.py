ca3da25d5a8ef0c4ba6f70931f82ec35f9fc9f1b
Markus Bader <markus.bader@mx-robotics.com>
2022-07-21 16:32:12 +0200

    tests with parameter added

--- tuw_gazebo/launch/empty.launch.py
+++ tuw_gazebo/launch/empty.launch.py
@@ -4,19 +4,25 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.substitutions import TextSubstitution
+from launch.actions import SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 
 
 def generate_launch_description():
 
+    room_arg    = DeclareLaunchArgument('room', default_value=TextSubstitution(text='empty.world'))
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_tuw_gazebo = get_package_share_directory('tuw_gazebo')
+    pkg_tuw_gazebo_models = get_package_share_directory('tuw_gazebo_models')
+    pkg_tuw_gazebo_plugins = get_package_share_directory('tuw_gazebo_plugins')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        model_path =  os.environ['GAZEBO_MODEL_PATH'] + ':' + pkg_tuw_gazebo_models + '/models'
+    else:
+        model_path =  pkg_tuw_gazebo_models + '/models'
 
 
     # Gazebo launch
@@ -26,11 +32,13 @@ def generate_launch_description():
         )
     )    
 
+    world_file = os.path.join(pkg_tuw_gazebo, 'worlds', 'empty.world')
 
     return LaunchDescription([
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
         DeclareLaunchArgument(
           'world',
-          default_value=[os.path.join(pkg_tuw_gazebo, 'worlds', 'empty.world'), ''],
+          default_value=world_file,
           description='SDF world file'),
         gazebo
     ])
