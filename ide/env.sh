f131ecd29c7a1a0c8c9a58fa6fe306c388cb789b
Markus Bader <markus.bader@tuwien.ac.at>
2022-11-09 11:31:05 +0100

    env.h with echo

---
+++ ide/env.sh
@@ -0,0 +1,4 @@
+echo "Trying to source gazebo"
+source /opt/ros/$ROS_DISTRO/setup.bash
+source ${HOME}/projects/ros/gazebo/ws00/devel/setup.bash
+source ${HOME}/projects/ros/gazebo/ws01/devel/setup.bash
