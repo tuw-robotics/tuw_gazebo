f587c5e4083f1272cb810a935867f6aebe580f68
Markus Bader <markus.bader@tuwien.ac.at>
2023-09-13 14:49:54 +0200

    robot_descriotion as extra launch

---
+++ tuw_gazebo_models/launch/robot_description.launch.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node
+from launch.actions import OpaqueFunction
+from launch.actions import SetLaunchConfiguration
+import xml.dom.minidom
+from launch.conditions import IfCondition
+
+import xacro
+
+def generate_launch_description():
+
+    use_sim_time     = LaunchConfiguration('use_sim_time',  default='true')
+    namespace_arg    = DeclareLaunchArgument('namespace',   default_value=TextSubstitution(text=''))
+    model_name_arg   = DeclareLaunchArgument('model_name',  default_value=TextSubstitution(text='robot0'))
+    robot_arg        = DeclareLaunchArgument('robot',       default_value=TextSubstitution(text='pioneer3dx'))
+
+    models_dir = get_package_share_directory('tuw_gazebo_models') + '/models'
+
+    def create_robot_description(context): 
+        xacro_file = os.path.join(get_package_share_directory('tuw_gazebo_models'), 'models', context.launch_configurations['robot'], 'main.xacro')
+        assert os.path.exists(xacro_file), "The main.xacro doesnt exist in "+str(xacro_file)
+        robot_description_config = xacro.process_file(xacro_file, 
+            mappings={  "namespace": context.launch_configurations['namespace'], 
+                        "models_dir": models_dir})
+        robot_desc = robot_description_config.toxml()
+        dom = xml.dom.minidom.parseString(robot_desc)
+        f = open("/tmp/robot_description.xml", "w")   ## for debugging only
+        f.write(dom.toprettyxml())
+        f.close()
+        return [SetLaunchConfiguration('robot_desc', robot_desc)]
+
+    create_robot_description_arg = OpaqueFunction(function=create_robot_description)
+
+    return LaunchDescription([
+        namespace_arg,
+        robot_arg,
+        create_robot_description_arg,
+        model_name_arg,
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            arguments=[LaunchConfiguration('robot_desc')],
+            namespace=[LaunchConfiguration('namespace')],
+            # this works but I will not use it at the moment
+            # remappings=[
+            #    ("/tf", "/r0/tf"),
+            #    ("/tf_static", "/r0/tf_static")
+            #],
+            parameters=[{
+                "use_sim_time": use_sim_time,
+                "robot_description": LaunchConfiguration('robot_desc')}],
+            output="screen"),
+    ])
